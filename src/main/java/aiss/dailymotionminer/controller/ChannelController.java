7061278d19063a924c4df13d369a1b01088fde4a
Adrián <adrgonutr@alum.us.es>
2026-05-04 15:55:03 +0200

    Final version v3.1: added http status

--- src/main/java/aiss/dailymotionminer/controller/ChannelController.java
+++ src/main/java/aiss/dailymotionminer/controller/ChannelController.java
@@ -3,9 +3,7 @@ package aiss.dailymotionminer.controller;
 import aiss.dailymotionminer.model.ChannelDTO;
 import aiss.dailymotionminer.service.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
@@ -17,9 +15,11 @@ public class ChannelController {
     ChannelService channelService;
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ChannelDTO getChannel(@PathVariable String id, @RequestParam (required = false, defaultValue = "10") int maxVideos, @RequestParam (required = false, defaultValue = "2") int maxComments) { return channelService.findChannelById(id, maxVideos, maxComments); }
 
     @PostMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
     public ChannelDTO postChannel(@PathVariable String id, @RequestParam (required = false, defaultValue = "10") int maxVideos, @RequestParam (required = false, defaultValue = "2") int maxComments) {
         ChannelDTO channelDTO=channelService.postChannelDTOById(id, maxVideos, maxComments);
         return channelDTO;
